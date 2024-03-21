37474f6c60e03496e21d05187531e20edb5cb7b4
Bohdan Tymofieienko <b.tymofieienko@gmail.com>
2024-03-21 13:41:08 +0100

    added user entity

--- src/main/java/com/bohdan/securitydemo/user/User.java
+++ src/main/java/com/bohdan/securitydemo/user/User.java
@@ -1,6 +1,10 @@
 package com.bohdan.securitydemo.user;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +14,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="appUser")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String lastname;
